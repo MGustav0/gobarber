48f39236d281fd9831289d5e8cb5c93c5d463db4
Gustavo Moreira <gustavo@gustavo-pc.WORKGROUP>
2020-03-27 19:41:43 -0500

    Página SignUp

--- frontendMobile/src/pages/SignUp/index.js
+++ frontendMobile/src/pages/SignUp/index.js
@@ -1,14 +1,56 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image } from 'react-native';
+
+import logo from '~/assets/logo.png';
 
 import Background from '~/components/Background';
 
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+
 // import { Container } from './styles';
 
-export default function SignUp() {
+export default function SignUn({ navigation }) {
   return (
     <Background>
-      <Text>SigUp</Text>
+      <Container>
+        <Image source={logo} />
+
+        <Form>
+          <FormInput
+            icon="person-outline"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Nome completo"
+          />
+
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Digite sua senha"
+          />
+
+          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => navigation.navigate('SignUp')}>
+          <SignLinkText>Criar conta</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }
